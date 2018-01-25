b4f42ef392beb5c40417f2c98a043562a58ab2f4
Chen <zdms1987@hotmail.com>
2018-01-25 09:02:14 -0500

    add file containing useful function for continum mechanic calculation

---
+++ cm.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+In package module provide basic support for continuum mechanics related
+calculations.
+All functions should be implemented in numpy.
+Usage of scipy package should be imported at function scope.
+"""
+
+import numpy as np
+
+def get_deviatoric_defgrad(_defgrad):
+    """calculate deviatoric deformation gradient with given defgrad"""
+    dim = _defgrad.shape[0]
+    return np.power(np.linalg.det(_defgrad), -1.0/dim)*_defgrad
+
+
+if __name__ == "__main__":
+    # module level demo 
+
+    # calculate deviatoric deformation gradient
+    defgrad = np.eye(3) + 1e-4*(np.ones(9, dtype=np.float64) - 2.*np.random.random(9)).reshape(3,3)
+    print("defgrad:\n{}\ndefgrad_D:\n{}\n".format(defgrad, get_deviatoric_defgrad(defgrad)))
