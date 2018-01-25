ec0a12329664c88705cd9c00a5c71887397cfb0e
Chen <zdms1987@hotmail.com>
2018-01-25 09:10:50 -0500

    remove export in init

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
-import cm
+"""
+daxm_voxel.py
+Data analyzing toolkit developed for extracting lattice strain and
+dislocation content from differential aperture X-ray microscopy (DAXM)
+data collected from synchrontron X-ray.
+"""
