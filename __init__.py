a4fe5183c6f8bc80bc74ae0306c8327b82766483
Chen <zdms1987@hotmail.com>
2018-01-25 13:37:00 -0500

    remove file name

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 """
-daxm_voxel.py
 Data analyzing toolkit developed for extracting lattice strain and
 dislocation content from differential aperture X-ray microscopy (DAXM)
 data collected from synchrontron X-ray.
